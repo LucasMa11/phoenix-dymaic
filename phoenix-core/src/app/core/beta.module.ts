19f5eee6130147ed50f260a30efedc9536a17b5b
Lucas Ma <mayh11@lenovo.com>
2019-07-26 14:25:47 +0800

    new solution

---
+++ phoenix-core/src/app/core/beta.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BetaRoutingModule } from './beta-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BetaRoutingModule
+  ]
+})
+export class BetaModule { }
---
+++ phoenix-core/src/app/core/prod.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProdRoutingModule } from './prod-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ProdRoutingModule
+  ]
+})
+export class ProdModule { }
